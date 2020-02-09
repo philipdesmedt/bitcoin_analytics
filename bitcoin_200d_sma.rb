5bf0e8c898e171654623815dc0e457984c8dba49
Philip De Smedt <philip.desmedt@gmail.com>
2020-02-09 11:42:17 +0000

    Simplify scripts with BitcoinAnalyser class

--- bitcoin_200d_sma.rb
+++ bitcoin_200d_sma.rb
@@ -1,12 +1,5 @@
-require 'csv' # frozen_string_literal: true
-FILE = 'btc.csv'
+require './bitcoin_analyser'
 
-bitcoin_data = CSV.parse(File.read(FILE), headers: true, col_sep: ',')
-total_price = 0
-bitcoin_data = bitcoin_data[bitcoin_data.length - 200..bitcoin_data.length]
-bitcoin_data.each { |data| total_price += data['PriceUSD'].to_f }
-
-sma = (total_price / bitcoin_data.length).round(2)
-puts "SMA #{bitcoin_data.length} days = $#{sma}"
-mm = (bitcoin_data[-1]['PriceUSD'].to_f / sma).round(2)
-puts "Mayer Multiple = #{mm}"
+analyser = BitcoinAnalyser.new
+puts "SMA 200 days = $#{analyser.calculate_moving_average(200)}"
+puts "Mayer Multiple = #{analyser.mayer_multiple}"
--- bitcoin_2y_ma_5_2y_ma.rb
+++ bitcoin_2y_ma_5_2y_ma.rb
@@ -1,12 +1,8 @@
-require 'csv' # frozen_string_literal: true
-FILE = 'btc.csv'
+require './bitcoin_analyser'
 
-bitcoin_data = CSV.parse(File.read(FILE), headers: true, col_sep: ',')
-total_price = 0
-bitcoin_data = bitcoin_data[bitcoin_data.length - 730..bitcoin_data.length]
-bitcoin_data.each { |data| total_price += data['PriceUSD'].to_f }
+analyser = BitcoinAnalyser.new
 
-sma = (total_price / bitcoin_data.length).round(2)
 puts 'Ideal time to sell BTC into fiat based on historical data is above 5*SMA(730)'
-puts "SMA #{bitcoin_data.length} days = $#{sma}"
-puts "5 * SMA #{bitcoin_data.length} days = $#{5 * sma}"
+sma = analyser.calculate_moving_average(730)
+puts "SMA 730 days = $#{sma}"
+puts "5 * SMA 730 days = $#{5 * sma}"
